e94acbc7a601e90f198d7ef861d66e652ed05231
mgwalsh <markusgwalsh@gmail.com>
2019-04-13 08:41:03 +0300

    Tanzania topsoil (0-20 cm) pH & OC data setup

--- TZ_pH_data.R
+++ TZ_pH_data.R
@@ -1,5 +1,5 @@
-# Tanzania pH data setup
-# M. Walsh, October 2017
+# Tanzania topsoil (0-20 cm) pH & OC data setup
+# M. Walsh, April 2019
 
 # Required packages
 # install.packages(c("downloader","rgdal","raster")), dependencies=TRUE)
@@ -17,14 +17,14 @@ setwd("./TZ_data")
 # download reference data
 download("https://www.dropbox.com/s/7vzb6fhrvga3lh2/TZ_pH.csv.zip?raw=1", "TZ_pH.csv.zip", mode="wb")
 unzip("TZ_pH.csv.zip", overwrite=T)
-phdat <- read.table("TZ_pH.csv", header=T, sep=",")
+sdat <- read.table("TZ_pH.csv", header=T, sep=",")
 
-# download Gtifs and stack in raster (note this is a big 800+ Mb download)
-download("https://www.dropbox.com/s/pshrtvjf7navegu/TZ_250m_2017.zip?raw=1", "TZ_250m_2017.zip", mode="wb")
-unzip("TZ_250m_2017.zip", overwrite=T)
+# download Gtifs and stack in raster (note this is a big 800MB+ download)
+download("https://www.dropbox.com/s/ejl3h62hojnhh3a/TZ_250m_2019.zip?raw=1", "TZ_250m_2019.zip", mode="wb")
+unzip("TZ_250m_2019.zip", overwrite=T)
 
-# download GeoSurvey prediction grids
-download("https://www.dropbox.com/s/3px2xh9l4a6b38g/TZ_GS_preds.zip?raw=1", "TZ_GS_preds.zip", mode="wb")
+# download latest GeoSurvey prediction grids
+download("https://www.dropbox.com/s/g4a80sddyo4o3i2/TZ_GS_preds.zip?raw=1", "TZ_GS_preds.zip", mode="wb")
 unzip("TZ_GS_preds.zip", overwrite=T)
 
 # stack grids
@@ -33,16 +33,16 @@ grids <- stack(glist)
 
 # Data setup ---------------------------------------------------------------
 # project reference data coords to grid CRS
-ph.proj <- as.data.frame(project(cbind(phdat$lon, phdat$lat), "+proj=laea +ellps=WGS84 +lon_0=20 +lat_0=5 +units=m +no_defs"))
-colnames(ph.proj) <- c("x","y")
-phdat <- cbind(phdat, ph.proj)
-coordinates(phdat) <- ~x+y
-projection(phdat) <- projection(grids)
+sdat.proj <- as.data.frame(project(cbind(phdat$lon, phdat$lat), "+proj=laea +ellps=WGS84 +lon_0=20 +lat_0=5 +units=m +no_defs"))
+colnames(sdat.proj) <- c("x","y")
+sdat <- cbind(sdat, sdat.proj)
+coordinates(sdat) <- ~x+y
+projection(sdat) <- projection(grids)
 
 # extract gridded variables at site locations
-phgrid <- extract(grids, phdat)
-phdat <- as.data.frame(cbind(phdat, phgrid))
+sgrid <- extract(grids, sdat)
+sdat <- as.data.frame(cbind(sdat, sgrid))
 
 # Write output file --------------------------------------------------------
 dir.create("Results", showWarnings=F)
-write.csv(phdat, "./Results/TZ_phdat.csv", row.names = FALSE)
+write.csv(phdat, "./Results/TZ_sdat.csv", row.names = FALSE)
